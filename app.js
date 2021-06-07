5fe8fa135da55ce38ce3bd09e128dd07db2cc2e5
NHatim <hatim.naimi@gmail.com>
2021-06-07 12:40:31 +0200

    jwt api login

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 require("dotenv").config();
 // import routes
 const userRoutes = require("./routes/user");
@@ -20,8 +20,10 @@ mongoose
 
 // middlewares
 app.use(morgan("dev"));
-app.use(bodyParser.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
+app.use(expressValidator())
 
 // routes middleware
 app.use("/api", userRoutes);
--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 let uuidv1 = require('uuidv1');
 const {errorHandler} = require('../helpers/dbErrorHandler')
+const jwt = require('jsonwebtoken')
+const expresJwt = require('express-jwt')
 
 exports.signup = (req, res) => {
     console.log("req.body", req.body);
@@ -18,3 +20,28 @@ exports.signup = (req, res) => {
         })
     });
 };
+
+exports.signin = (req, res) =>{
+    const {email, password} = req.body
+    User.findOne({email}, (err,user) =>{
+        if(err || !user){
+            return res.status(400).json({
+                err: 'User with that e-mail doesn\'t exist. Please signup'
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error : 'Email and password don\'t match'
+            })
+        }
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+        res.cookie('t', token, {expire: new Date() + 9999})
+        const {_id, name, email, role} = user
+        return res.json({token, user : {_id, email, name, role}})
+    }) 
+}
+
+exports.signout = (req, res) =>{
+    res.clearCookie('t')
+    res.json({message: 'Signout'})
+}
